5a7871bc4a995c32d2ed3a555a521f38b79a95c0
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-10-13 18:17:25 +0200

    Necessary changes

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='calculator',
-    version='1.0',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'calculator=calculator.__main__:main',
-        ],
-    },
+    name="calculator",
+    version="1.0",
+    packages=["calculator"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
 )
