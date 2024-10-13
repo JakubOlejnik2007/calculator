8dfe64abb20f3c3fd165e5d33ca7eb7bce9045dc
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-10-13 18:20:42 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
@@ -6,8 +7,15 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="calculator",
     version="1.0",
-    packages=["calculator"],
+    packages=find_packages(),
     long_description=long_description,
-    long_description_content_type="text/markdown",
-
+    long_description_content_type='text/markdown',
+    author="Jakub Olejnik",
+    author_email="jacobole2000@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
