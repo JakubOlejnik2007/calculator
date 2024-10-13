7db81d683c1ebf0a57867d177ffe60fdc97fffec
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-10-13 18:25:14 +0200

    Change project name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="calculator",
+    name="calculator-jakub",
     version="1.0",
     packages=find_packages(),
     long_description=long_description,
